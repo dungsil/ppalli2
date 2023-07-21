ff86af827b2aaffbffec4561028413394afeb0a0
김용건 (Kim Younggeon) <opensource@younggeon.kim>
2023-07-21 21:33:31 +0900

    chore: Kotlin v1.9.0 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  kotlin("jvm") version "1.8.21"
-  kotlin("plugin.jpa") version "1.8.21"
-  kotlin("plugin.spring") version "1.8.21"
+  kotlin("jvm") version "1.9.0"
+  kotlin("plugin.jpa") version "1.9.0"
+  kotlin("plugin.spring") version "1.9.0"
   id("org.jetbrains.dokka") version "1.8.20"
 
   id("org.springframework.boot") version "3.1.1"
