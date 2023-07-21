49a1edc7f9ee5e37cf26930c09b77f098f8ce02e
김용건 (Kim Younggeon) <opensource@younggeon.kim>
2023-07-21 22:26:11 +0900

    chore(deps): Spring boot v3.1.2 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
   kotlin("plugin.spring") version "1.9.0"
   id("org.jetbrains.dokka") version "1.8.20"
 
-  id("org.springframework.boot") version "3.1.1"
-  id("io.spring.dependency-management") version "1.1.0"
+  id("org.springframework.boot") version "3.1.2"
+  id("io.spring.dependency-management") version "1.1.2"
 }
 
 group = "ppalli"
